4db45eaf7dd446767635c182cd79880700160d5a
yuna <rndbsk9@swu.ac.kr>
2025-04-18 14:33:56 +0900

    fix: ground 레이어 없이 바닥 감지, 공중 판정 버그 해결

--- TowerOfTime/Assets/Scripts/Player/States/JumpState.cs
+++ TowerOfTime/Assets/Scripts/Player/States/JumpState.cs
@@ -12,7 +12,10 @@ public class JumpState : IState
     }
     public void Enter()
     {
-        _player.TryJump();
+        if (!_player.TryJump())
+        {
+            _player.ChangeState(_player.IdleState); // 점프 실패 시 Idle 상태로 전환
+        }
     }
 
     public void Update()
