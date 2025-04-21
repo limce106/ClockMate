a32a3e220bf3508d3f5277c81e8c4a47776ea643
konguksu <rndbsk9@swu.ac.kr>
2025-04-21 22:06:23 +0900

    refactor: 캐릭터 로직 전반 리팩토링 및 InputSystem 적용, 이동 및 점프 관련 버그 수정

--- TowerOfTime/Assets/Scripts/Player/Milli.cs
+++ TowerOfTime/Assets/Scripts/Player/Milli.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Milli : PlayerBase
+public class Milli : CharacterBase
 {
     
 }
--- TowerOfTime/Assets/Scripts/Player/States/StateMachine.cs
+++ TowerOfTime/Assets/Scripts/Player/States/StateMachine.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class StateMachine
 {
     public IState CurrentState {get; private set;}
 
-    public void ChangeState(IState newState)
+    public void ChangeStateTo(IState newState)
     {
+        if (CurrentState == newState || !CanTransition(CurrentState, newState)) return;
+        
         CurrentState?.Exit(); // 이전 상태 정리
         CurrentState = newState; // 새 상태로 교체
         CurrentState?.Enter(); // 새 상태 준비
@@ -16,5 +14,19 @@ public class StateMachine
     public void Update()
     {
         CurrentState?.Update();
+    }
+
+    public void FixedUpdate()
+    {
+        CurrentState?.FixedUpdate();
+    }
+    
+    private bool CanTransition(IState from, IState to)
+    {
+        // Move는 Idle에서만 가능
+        if (!(from is IdleState) && to is WalkState)
+            return false;
+
+        return true;
     }
 }
