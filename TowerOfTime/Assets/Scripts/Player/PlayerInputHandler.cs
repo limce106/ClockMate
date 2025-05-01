bdd53883c415b65cb17d0f2a5e9d087d10e5c8a9
konguksu <rndbsk9@swu.ac.kr>
2025-05-01 21:00:00 +0900

    feat: Falling/MovingBlock, PressurePlate 추가, 중력 수정, Player Input 관련 구조 수정

--- TowerOfTime/Assets/Scripts/Player/PlayerInputHandler.cs
+++ TowerOfTime/Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using static Define.Character;
+using InputAction = UnityEngine.InputSystem.InputAction;
 
 /// <summary>
-/// 사용자 입력을 감지하고 PlayerBase로 전달하는 입력 처리기.
+/// 사용자 입력을 감지하는 입력 처리기.
 /// Unity Input System 기반
 /// </summary>
 [RequireComponent(typeof(CharacterBase))]
@@ -12,18 +14,23 @@ public class PlayerInputHandler : MonoBehaviour
 {
     private CharacterBase _character;
     private PlayerInputActions _inputActions;
+    private bool _isMoving;
+
+    private Dictionary<CharacterAction, bool> _actionsAvailable;
 
     private void Awake()
     {
         _character = GetComponent<CharacterBase>();
         _inputActions = new PlayerInputActions();
-        
-        InitInputActions();
-    }
+        _isMoving = false;
 
-    void Update()
-    {
-        HandleMoveInput();
+        _actionsAvailable = new Dictionary<CharacterAction, bool>
+        {
+            { CharacterAction.Move, true },
+            { CharacterAction.Jump, true }
+        };
+
+        InitInputActions();
     }
 
     private void OnEnable()
@@ -41,28 +48,53 @@ public class PlayerInputHandler : MonoBehaviour
         _inputActions.Player.Jump.performed += OnJumpPressed;
         _inputActions.Player.Interact.performed += OnInteractPressed;
         _inputActions.Player.Ability.performed += OnAbilityPressed;
+        _inputActions.Player.Move.performed += OnMovePressed;
     }
 
-    private void HandleMoveInput()
+    private void FixedUpdate()
     {
+        if (!_isMoving) return;
+
         Vector2 input = _inputActions.Player.Move.ReadValue<Vector2>();
-        _character.UpdateMoveDirection(new Vector3(input.x, 0, input.y));
-        _character.TryChangeState(_character.MoveState);
+        _character.Move(new Vector3(input.x, 0, input.y));
+        if (input.sqrMagnitude < 0.01f)
+        {
+            _isMoving = false;
+            _character.ChangeState(_character.IdleState);
+        }
     }
+
+    private void OnMovePressed(InputAction.CallbackContext context)
+    {
+        if (!_actionsAvailable[CharacterAction.Move]) return;
+        _isMoving = true;
+        _character.ChangeState(_character.WalkState);
+    }
+
 
     private void OnJumpPressed(InputAction.CallbackContext context)
     {
-        _character.TryChangeState(_character.JumpState);
+        if (!_actionsAvailable[CharacterAction.Jump] || !_character.CanJump()) return;
+        _character.ChangeState(_character.JumpState);
+        _character.PerformJump();
     }
     
     private void OnInteractPressed(InputAction.CallbackContext context)
     {
-        //_character.TryChangeState(_character.InteractState);
+        //_character.ChangeState(_character.InteractState);
     }
     
     private void OnAbilityPressed(InputAction.CallbackContext context)
     {
-        //_character.TryChangeState(_character.AbilityState);
+        //_character.ChangeState(_character.AbilityState);
+    }
+
+    public void ActivateInputActions(HashSet<CharacterAction> availableActions)
+    {
+        foreach (CharacterAction action in _actionsAvailable.Keys)
+        {
+            _actionsAvailable[action] = availableActions.Contains(action);
+        }
     }
     
 }
---
+++ TowerOfTime/Assets/Scripts/Util/Define.cs
@@ -0,0 +1,50 @@
+﻿namespace Define
+{
+    public static class Character
+    {
+        public enum CharacterName
+        {
+            Hour,
+            Milli
+        }
+        
+        public enum CharacterAction
+        {
+            Move,
+            Jump
+        }
+    }
+    public static class UI
+    {
+        public enum UIType
+        {
+            FullScreen, // 전체화면 UI
+            Windowed // 화면 일부에만 표시되는 UI
+        }
+    }
+
+    public static class Map
+    {
+        public enum MapName
+        {
+            None = 0,
+            Desert,
+            Glacier,
+            Forest,
+            ClockTower
+        }
+    }
+
+    public static class Block
+    {
+        public enum MovingDirection
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            Forward,
+            Backward
+        }
+    }
+}
