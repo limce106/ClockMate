c326b07453ead0746ec2d306d26250a4e5638711
limce106 <limce106@naver.com>
2025-05-08 00:52:40 +0900

    Feat: Launch Player Upward
    - 플레이어를 위로 띄우는 환풍기 구현
    
    - 환풍기와 플레이어의 거리가 멀어질수록 바람의 세기는 약해짐
    - 플레이어가 환풍기의 X, Z 범위에서 벗어나면 힘을 가하지 않도록 처리
    
    - 플레이어 점프 관련 문제 처리 필요
    - 플레이어가 점프 후 떨어지는 것이 아닌 원래 높이로 되돌아가게 해야 함
    - 점프 후 땅에 닿지 않기 때문에 State가 변하지 않아, 점프가 끝났음에도 계속 JumpState로 남아있는 문제가 있음

--- ClockMate/Assets/Scripts/Desert/AirFan/FrontAirFanTrigger.cs
+++ ClockMate/Assets/Scripts/Desert/AirFan/FrontAirFanTrigger.cs
@@ -19,4 +19,16 @@ public class FrontAirFanTrigger : MonoBehaviour
             GetComponentInParent<AirFan>().SetMilliInTrigger(false);
         }
     }
+
+    public bool IsPlayerInXZRange(Vector3 playerPos)
+    {
+        BoxCollider frontBoxCollider = GetComponent<BoxCollider>();
+        Vector3 localPoint = frontBoxCollider.transform.InverseTransformPoint(playerPos);
+        Vector3 halfSize = frontBoxCollider.size * 0.5f;
+
+        bool inXZRange = Mathf.Abs(localPoint.x) <= halfSize.x &&
+                         Mathf.Abs(localPoint.z) <= halfSize.z;
+
+        return inXZRange;
+    }
 }
