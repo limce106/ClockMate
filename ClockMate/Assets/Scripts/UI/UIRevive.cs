ce13415b91663fd65ee47c91d2c917abb5933525
konguksu <rndbsk9@swu.ac.kr>
2025-07-03 18:33:59 +0900

    feat: refactor lifeManager and synchronize
    - CharacterLifeManager -> StageLifeManager로 이름 변경
    - CharacterBase에 CharacterName enum 프로퍼티추가
    - 테스트의 경우 스테이지 id를 inspector에서 할당 가능하도록 수정
    - RPC_Revive, RPC_HandleDeath 메서드 RPCManager에 추가
    - 부활 UI 수정 + 부활을 E키 한번 누르는 것에서 10번 연타로 변경

--- ClockMate/Assets/Scripts/UI/UIRevive.cs
+++ ClockMate/Assets/Scripts/UI/UIRevive.cs
@@ -1,8 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static Define.Character;
 
 public class UIRevive : UIBase
 {
+    [SerializeField] private Text text;
+    [SerializeField] private Image progress;
+    [SerializeField] private Image characterImage;
+    [SerializeField] private Image inputKeyImage;
 
+    public void SetUI(bool isDeadPlayer, CharacterName deadCharacter)
+    {
+        SetProgress(0);
+        
+        if (isDeadPlayer)
+        {
+            text.text = "대기중";
+            inputKeyImage.enabled = false;
+        }
+        else
+        {
+            text.text = "살려주기";
+            inputKeyImage.enabled = true;
+        }
+        
+        string imgName = deadCharacter == CharacterName.Hour ? "HourIcon" : "MilliIcon";
+        characterImage.sprite = Resources.Load<Sprite>("UI/Sprites/" + imgName);
+    }
+
+    public void SetProgress(float amount)
+    {
+        this.progress.fillAmount = amount;
+    }
 }
