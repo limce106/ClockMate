458c4c7cfeaf4006999733db808c78016fd60283
limce106 <limce106@naver.com>
2025-07-12 00:00:24 +0900

    fix: Player Spawing as Rolling Stone
    - 각 클라이언트가 방에 입장하면 돌이 2개 생성되는 문제가 발생함. 정상적인 위치에 스폰되는 돌 1개, 플레이어 스폰 위치에 스폰되는 돌 1개로, 테스트 서버 버튼 클릭 시 아워/밀리 프리팹이 아닌 돌 프리팹이 생성되는 문제.
    - PhotonNetwork.PrefabPool = rollingStonePool; 코드 삭제하여 해결.
    PhotonNetwork.PrefabPool에 rollingStonePool을 할당하면, 포톤이 모든 Instantiate 호출 시 그 풀을 사용해 오브젝트를 생성함. 그래서 플레이어 생성 시에도 돌 프리팹을 사용하여 돌이 생성됨. 삭제 후 기본 프리팹 관리로 돌아가서 플레이어가 정상 생성됨
    
    - Forest 씬 추가
    - 숲 Puzzle2 프리팹 추가. 해당 프리팹 안에는 RollingStoneController가 있음.

--- ClockMate/Assets/02.Scripts/Forest/Puzzle2/RollingStoneSpawner.cs
+++ ClockMate/Assets/02.Scripts/Forest/Puzzle2/RollingStoneSpawner.cs
@@ -2,7 +2,6 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
 
 public class RollingStoneSpawner : MonoBehaviourPunCallbacks
