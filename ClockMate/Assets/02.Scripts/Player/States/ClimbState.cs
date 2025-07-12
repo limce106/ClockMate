312422984db1add047528724291017ccce886702
limce106 <limce106@naver.com>
2025-07-12 20:11:30 +0900

    feat: Climable Object
    - IInteractable을 상속받은 기어올라갈 수 있는 오브젝트 스크립트 구현
    - 해당 스크립트를 콩나무 프리팹에 적용
    
    - 상호작용 가능한 상태이면 E 이미지와 "올라가기" 텍스트를 띄운다.
    - 상호작용하면 플레이어를 ClimbState 상태로 전환

---
+++ ClockMate/Assets/02.Scripts/Player/States/ClimbState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbState : IState
+{
+    private readonly CharacterBase _character;
+
+    public ClimbState(CharacterBase character) => _character = character;
+    public void Enter()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+
+    }
+}
