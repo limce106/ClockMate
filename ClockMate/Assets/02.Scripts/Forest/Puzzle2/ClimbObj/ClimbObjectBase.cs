225915818479c16a5f056cbb5256ad63ca6e4c97
limce106 <limce106@naver.com>
2025-07-20 21:11:39 +0900

    chore: Delete IClimableObject
    - IClimableObject를 삭제하고 AttachTo 함수를 ClimbObjectBase의 추상 함수로 만듦

--- ClockMate/Assets/02.Scripts/Forest/Puzzle2/ClimbObj/ClimbObjectBase.cs
+++ ClockMate/Assets/02.Scripts/Forest/Puzzle2/ClimbObj/ClimbObjectBase.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class ClimbObjectBase : MonoBehaviour, IClimableObject
+public abstract class ClimbObjectBase : MonoBehaviour, IInteractable
 {
     protected UIManager _uiManager;
     protected UINotice _uiNotice;
--- ClockMate/Assets/02.Scripts/Forest/Puzzle2/ClimbObj/IClimableObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IClimableObject : IInteractable
-{
-    void AttachTo(CharacterBase character);
-}
