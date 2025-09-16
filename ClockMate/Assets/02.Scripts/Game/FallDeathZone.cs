1611e09f6e80f5d763c9ec698b20b3c8c96fb450
konguksu <rndbsk9@swu.ac.kr>
2025-09-16 20:31:12 +0900

    feat: modify fall death zone logic
    - 현재 스테이지의 로딩 위치에 fall death zone과 충돌한 캐릭터 순간 이동
    - 순간 이동 시 임시 이펙트 재생

--- ClockMate/Assets/02.Scripts/Game/FallDeathZone.cs
+++ ClockMate/Assets/02.Scripts/Game/FallDeathZone.cs
@@ -1,16 +1,22 @@
 using DefineExtension;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FallDeathZone : MonoBehaviour
 {
+    [SerializeField] ParticleSystem poofEffect;
     private void OnTriggerEnter(Collider other)
     {
         if(other.IsPlayerCollider())
         {
             CharacterBase character = other.gameObject.GetComponentInParent<CharacterBase>();
-            character.ChangeState<DeadState>();
+            //character.ChangeState<DeadState>();
+            Vector3 loadPosition = GameManager.Instance.CurrentStage.LoadPositions[character.Name];
+            character.transform.position = loadPosition;
+            if (poofEffect != null)
+            {
+                Instantiate(poofEffect, loadPosition, Quaternion.identity);
+            }
+            character.ChangeState<IdleState>();
         }
     }
 }
