cdd3224efe9603eb79f82956f4a148fa323072f9
limce106 <limce106@naver.com>
2025-10-05 12:26:07 +0900

    chore: add ILaunchStrategy description

--- ClockMate/Assets/02.Scripts/Desert/Puzzle2/ILaunchStrategy.cs
+++ ClockMate/Assets/02.Scripts/Desert/Puzzle2/ILaunchStrategy.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 
 public interface ILaunchStrategy
 {
+    /// <summary>
+    /// 비행 가능 여부
+    /// </summary>
     bool CanLaunch(Milli milli, AirFan airFan);
+    /// <summary>
+    /// 비행 중단 조건 달성 여부
+    /// </summary>
     bool ShouldStopFlying(Milli milli, Rigidbody milliRb, AirFan airFan);
+    /// <summary>
+    /// 플레이어 비행시키기
+    /// </summary>
     void Launch(Milli milli, Rigidbody milliRb, AirFan airFan);
+    /// <summary>
+    /// 플레이어 비행 코루틴
+    /// </summary>
     IEnumerator LaunchCoroutine(Milli milli, Rigidbody milliRb, AirFan airFan);
 }
