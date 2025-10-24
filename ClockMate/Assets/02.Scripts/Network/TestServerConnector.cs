bfc75abbc2dbef126204cc6a534742cfe1dcfd7f
konguksu <rndbsk9@swu.ac.kr>
2025-10-25 01:21:14 +0900

    feat: allow test server choose current stage
    - 테스트 서버 접속 시 마스터 GameManager의 CurrentStage를 기존 세이브 데이터를 사용하여 설정할지 여부와 미사용 시 적용할 스테이지 ID 지정 가능
    - 스테이지 ID를 지정할 경우 세이브 데이터도 해당 스테이지 ID 기준으로 갱신

--- ClockMate/Assets/02.Scripts/Network/TestServerConnector.cs
+++ ClockMate/Assets/02.Scripts/Network/TestServerConnector.cs
@@ -7,6 +7,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 using ExitGames.Client.Photon;
+using UnityEngine.Serialization;
 
 public class TestServerConnector : MonoBehaviourPunCallbacks
 {
@@ -24,6 +25,10 @@ public class TestServerConnector : MonoBehaviourPunCallbacks
 
     [Header("BGM On/Off")]
     public bool isBgmOn = false;
+    
+    [Header("Current Stage")]
+    public bool useExistingData = true;
+    public int stageId = 1; // save data를 사용하지 않을 시 설정할 스테이지 id
 
     private void Start()
     {
@@ -108,6 +113,11 @@ public class TestServerConnector : MonoBehaviourPunCallbacks
             var hour = PhotonNetwork.Instantiate("Characters/Hour", hourSpawnPos, Quaternion.identity);
             GameManager.Instance.RegisterCharacter(Character.CharacterName.Hour, hour.GetComponent<CharacterBase>());
             GameManager.Instance?.SetSelectedCharacter(Character.CharacterName.Hour);
+            if (!useExistingData)
+            {
+                SaveManager.Instance.Save(stageId);   
+            }
+            GameManager.Instance?.LoadExistingSaveData();
         }
         else
         {
