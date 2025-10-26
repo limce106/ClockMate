b690f19e7b756b0c180c0551af1eee10ea00f31f
limce106 <limce106@naver.com>
2025-10-27 00:46:31 +0900

    feat: cutscene trigger field
    - 전투매니저에 컷씬 트리거를 끄고 킬 수 있는 isCutSceneTriggerOn 추가. 테스트를 위해 컷씬 재생을 끄고 싶은 경우 체크 해제. 실제 플레이를 위해서는 체크 시 정상 작동하도록 구현
    - 전투 매니저 변수 설명을 위한 헤더 추가
    - 전투 입장 컷씬 재생 시 로딩창이 없어졌는지 확인함. 테스트 시에는 로딩창이 없어서 무한 대기함. 따라서 로딩 중인지 확인 후 로딩창이 없어지길 기다리도록 수정.
    - 또한, 테스트 시에는 두 플레이어 입장 시간 차가 있기 때문에 모두 입장했는지 확인 후 재생하도록 보완

--- ClockMate/Assets/02.Scripts/ClockTower/ClockTowerEntranceTrigger.cs
+++ ClockMate/Assets/02.Scripts/ClockTower/ClockTowerEntranceTrigger.cs
@@ -1,4 +1,5 @@
 using DefineExtension;
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,14 +10,24 @@ public class ClockTowerEntranceTrigger : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(!PhotonNetwork.IsMasterClient) return;
         if (_triggered) return;
+        if (!BattleManager.Instance.isCutSceneTriggerOn) return;
 
         StartCoroutine(WaitAndStartCutScene());
     }
 
     private IEnumerator WaitAndStartCutScene()
     {
-        yield return new WaitUntil(() => !LoadingManager.Instance._isLoading);
+        if(PhotonNetwork.CurrentRoom.PlayerCount != 2)
+        {
+            yield return new WaitUntil(() => PhotonNetwork.CurrentRoom.PlayerCount == 2);
+        }
+
+        if (LoadingManager.Instance.isLoading)
+        {
+            yield return new WaitUntil(() => !LoadingManager.Instance.isLoading);
+        }
 
         CutsceneSyncManager.Instance.PlayForAll(
             "ClockTowerEntrance",
