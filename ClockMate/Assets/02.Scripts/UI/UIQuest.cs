1220a973dcb46caca0430f8c1ccd446b9cd08b57
limce106 <limce106@naver.com>
2025-11-06 15:47:59 +0900

    feat: set quest name sprite using image path from data table
    - 데이터 테이블에 퀘스트 이름 이미지 UI 경로 열 추가
    - 데이터 테이블에서 받아온 경로로 이미지를 찾고 스프라이트를 설정하도록 구현

--- ClockMate/Assets/02.Scripts/UI/UIQuest.cs
+++ ClockMate/Assets/02.Scripts/UI/UIQuest.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIQuest : UIBase
 {
-    public TMP_Text quest;
+    public TMP_Text questTxt;
+    public Image questName;
     public Coroutine showQuestCoroutine;
 
     private const float duration = 5f;
@@ -19,16 +21,19 @@ public class UIQuest : UIBase
 
     private IEnumerator ShowQuestCoroutine()
     {
-        SetQuestText();
+        SetQuest();
         yield return new WaitForSeconds(duration);
 
         gameObject.SetActive(false);
         showQuestCoroutine = null;
     }
 
-    private void SetQuestText()
+    private void SetQuest()
     {
+        // TODO 퀘스트 이름 이미지 애셋 추가 후 주석 풀기
+
         string currentQuest = "";
+        //Sprite questNameAsset = null;
 
         if(SceneManager.GetActiveScene().name == "ClockTower")
         {
@@ -36,6 +41,7 @@ public class UIQuest : UIBase
             Where(data => data.PlayerAttackType == BattleManager.Instance.playerAttackType).First<LDPlayerAttackQuest>();
 
             currentQuest = GameManager.Instance.GetLocalCharacter().CompareTag("Hour") ? playerAttackQuest.HourQuest : playerAttackQuest.MilliQuest;
+            //questNameAsset = Resources.Load<Sprite>(playerAttackQuest.QuestNameImgPath);
         }
         else
         {
@@ -43,8 +49,10 @@ public class UIQuest : UIBase
             Where(data => data.ID == GameManager.Instance.CurrentStage.ID).First<LDPuzzleQuest>();
 
             currentQuest = GameManager.Instance.GetLocalCharacter().CompareTag("Hour") ? puzzleQuest.HourQuest : puzzleQuest.MilliQuest;
+            //questNameAsset = Resources.Load<Sprite>(puzzleQuest.QuestNameImgPath);
         }
 
-        quest.text = currentQuest;
+        questTxt.text = currentQuest;
+        //questName.sprite = questNameAsset;
     }
 }
