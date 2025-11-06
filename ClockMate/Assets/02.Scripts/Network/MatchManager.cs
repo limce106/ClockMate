c2f1e6e427c1e7f476c7fd8f882779da4dc2494e
limce106 <limce106@naver.com>
2025-11-06 15:57:02 +0900

    chore: enable ready only when current room has 2 players
    - 플레이어 접속 화면에서 방에 두 명 모두 들어왔을 때만 준비완료가 가능하도록 버그 수정

--- ClockMate/Assets/02.Scripts/Network/MatchManager.cs
+++ ClockMate/Assets/02.Scripts/Network/MatchManager.cs
@@ -48,7 +48,7 @@ public class MatchManager : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && PhotonNetwork.InRoom)
+        if (Input.GetKeyDown(KeyCode.E) && PhotonNetwork.CurrentRoom.PlayerCount == 2)
         {
             int slotNum = FindMySlotNum();
             if (slotNum != -1)
