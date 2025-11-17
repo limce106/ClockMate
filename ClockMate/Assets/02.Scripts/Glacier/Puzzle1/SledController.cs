b1267223cf401e9a866dc1c131fdd0e80da9fdc1
konguksu <rndbsk9@swu.ac.kr>
2025-11-17 17:58:29 +0900

    fix: use Go instead of particleSystem

--- ClockMate/Assets/02.Scripts/Glacier/Puzzle1/SledController.cs
+++ ClockMate/Assets/02.Scripts/Glacier/Puzzle1/SledController.cs
@@ -18,7 +18,7 @@ public class SledController : MonoBehaviourPunCallbacks, IPunObservable
     [SerializeField] private string landSfxKey = "sled_ground_hit";
     [SerializeField] private string splashSfxKey = "water_splash";
     [SerializeField] private float sfxVolume = 1.0f;
-    [SerializeField] private ParticleSystem landVfx;
+    [SerializeField] private GameObject landVfxGo;
     [SerializeField] private ParticleSystem splashVfx;
     
     private Rigidbody _rb;
@@ -33,7 +33,7 @@ public class SledController : MonoBehaviourPunCallbacks, IPunObservable
         {
             if (value)
             {
-                landVfx.gameObject.SetActive(true);
+                landVfxGo.SetActive(true);
                 SoundManager.Instance.PlaySfx(key: landSfxKey, pos: transform.position, volume: sfxVolume, sync: true);
             }
             _isGrounded = value;
