8ee4a50e30358dd89b6b7d732c8d7c024be3750d
limce106 <limce106@naver.com>
2025-11-18 16:10:12 +0900

    chore: play battle bgm after battle field cutscene ended
    - 전투 컷씬 후 브금 켜지지 않는 문제 해결

--- ClockMate/Assets/02.Scripts/ClockTower/BattleFieldTrigger.cs
+++ ClockMate/Assets/02.Scripts/ClockTower/BattleFieldTrigger.cs
@@ -66,6 +66,7 @@ public class BattleFieldTrigger : MonoBehaviourPun
                     0f,
                     () =>
                     {
+                        GameManager.Instance.PlayMapBgm();
                         BattleManager.Instance.StartBattle();
                         photonView.RPC(nameof(RPC_ShowBattleUI), RpcTarget.All);
                     }
